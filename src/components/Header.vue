0c56966b12b46ece85e8e89763e06c86402c6aed
stfDeveloper <sstefancosmin@icloud.com>
2022-01-31 19:05:53 +0100

    Blank

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    header
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
